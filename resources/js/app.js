89c5838b789429f7711dec95ab06157d80a348ee
ElipticNight <secondary@aidan-byrne.org.uk>
2020-12-05 20:02:05 +0000

    added vue with some test components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+
+import testone from './components/app'
+import testtwo from './components/test2'
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        testone,
+        testtwo
+    }
+})
