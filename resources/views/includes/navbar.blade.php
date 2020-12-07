f46fbd67520bcc6baa8fcfa51974ca9970827dc9
ElipticNight <secondary@aidan-byrne.org.uk>
2020-12-07 08:57:03 +0000

    created basic navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,1 +1,7 @@
-navbar
+<div class="navbar">
+    <ul class="navbar-links">
+        <li><a href="#">one</a></li>
+        <li><a href="#">two</a></li>
+        <li><a href="#">three</a></li>
+    </ul>
+</div>
--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -5,13 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>
+        <link href="{{ asset('css/app.css') }}" type="text/css" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
-            @include('includes/navbar')
-            @yield('content')
+            <div class="header">
+                @include('includes/navbar')
+            </div>
+            @yield('content1')
         </div>
     </body>
 </html>
